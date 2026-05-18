8ce2d459f5cb8304907b5562e30def6ec9d7f8f9
MT 德澄 遼仁 <ryoto.tokuzumi@metateam.co.jp>
2026-05-18 15:28:02 +0900

    何度もすみません。controller及びserviceの不要な差分を削除しました(3)

--- src/main/java/jp/co/metateam/library/service/BookMstService.java
+++ src/main/java/jp/co/metateam/library/service/BookMstService.java
@@ -19,7 +19,7 @@ import jp.co.metateam.library.repository.BookMstRepository;
 public class BookMstService {
 
     private final BookMstRepository bookMstRepository;
-
+    
     @Autowired
     public BookMstService(BookMstRepository bookMstRepository){
         this.bookMstRepository = bookMstRepository;
